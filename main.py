7ffaedb68732352007b441008c17dc129827573d
J. M. Cafferata <jmcafferata@gmail.com>
2023-04-20 19:52:41 -0300

    Revert "Redroy"

--- main.py
+++ main.py
@@ -15,33 +15,30 @@ timezone = pytz.timezone('America/Argentina/Buenos_Aires')
 
 app = Flask(__name__)
 
+# Set Google Cloud Storage credentials (si vas a correr el programa localmente, sacá esto)
+
+# Estas dos funciones son para leer y escribir archivos en GCS (si vas a correr el programa localmente, sacalas)
+
 def check_and_compute_cosine_similarity(x, message_vector):
     x = np.array(literal_eval(x), dtype=np.float64)  # Convert x to float64
     return cosine_similarity(x, message_vector)
 
+
 def get_information(message,is_admin):
     # Set OpenAI API key
-    openai.api_key_path = "openai_api_key.txt"
-   
-    # read the output.json file
-    with open('vectorized/output.json', 'r', encoding='utf-8') as f:
-        data = f.read()
+    openai.api_key_path = "ai_key.txt"
+    # open vendors.csv
+    THIS_FOLDER = Path(__file__).parent.resolve()
 
-    # convert the json string to a list of dictionaries
-    data = literal_eval(data)
-
-    # create a dataframe with the data
-    df = pd.DataFrame(data)
-
-    # create a new column with the embeddings converted to string
-    df['embedding'] = df['embedding'].apply(lambda x: str(x))
-
-    mensajes_sim = df[['metadata', 'text_chunk', 'embedding']].copy()
-
-
+    # THE JUICE
+    # activá esto si vas a correr el programa localmente. si no, desactivá esto y activá las dos líneas de abajo
+    mensajes_file = THIS_FOLDER / "mensajes.csv"
+    mensajes_df = pd.read_csv(mensajes_file, sep='|', encoding='utf-8')
+    mensajes_sim = mensajes_df
+    # get embedding for message
     message_vector = get_embedding(message, 'text-embedding-ada-002')
 
-    # Calculate cosine similarity between the message vector and the vectors in the output.json
+    # Calculate cosine similarity
     mensajes_sim['similarity'] = mensajes_sim['embedding'].apply(lambda x: check_and_compute_cosine_similarity(x, message_vector))
     print('similarity: ', mensajes_sim['similarity'])
 
@@ -53,25 +50,25 @@ def get_information(message,is_admin):
     
     mensajes = ''
 
-    for index, row in mensajes_sim[['metadata', 'text_chunk']].head(3).iterrows():
-        mensajes += str(row['metadata']) + ' - ' + str(row['text_chunk']) + '\n\n'
+    for index, row in mensajes_sim[['fecha', 'mensaje']].head(30).iterrows():
+        mensajes += str(row['fecha']) + ' - ' + str(row['mensaje']) + '\n\n'
         
-    system = """Sos Roy Cortina, el diputado argentino por el Partido Socialista. Hablás en argentino buena onda y canchero! Usás emojis irónicos y graciosos! Aguante Buenos Aires, loco! Sos el gemelo digital de Roy. Contestá con buena onda teniendo en cuenta tus ideas socialistas y tu amor por la gente. Hablás en argentino usando vos, che, pibe, etc.
-    
+    system = """Sos Count Basie, un bot argentino, buena onda, amable e irreverente (con dialecto argentino) que recibe y entrega información sobre la orquesta de jazz "Brillo Urbano Big Band".
+    La gente te da información o te pide información y vos respondés acordemente. 
     Hoy es """+now.strftime("%A %d/%m/%Y %H:%M:%S")+"""
     
-    Acá hay transcripciones de cosas que dijiste:
+    Usar los siguientes datos para responder:
 
     """+mensajes+"""
-        
+    
+    Hablás en tono argentino (a menos que te hablen en un idioma que no sea castellano) y amigable. Usá emojis irónicos y graciosos! Si alguien te habla en otro lenguaje, como francés o inglés, respondé en el otro lenguaje.
+    
     En tu respuesta, envolvé los links en tag <a class=link href=[el link]> y los numeros de telefono en tags <span class=phone>."""
     
     prompt = message
-
-    print(system)
     
     response = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": system},
         {"role": "user", "content": prompt}],
@@ -79,6 +76,16 @@ def get_information(message,is_admin):
         
     print(response['choices'][0]['message']['content'])
     response = response['choices'][0]['message']['content']
+
+
+    #activá esto si vas a correr el programa localmente. si no, desactivá esto y activá las dos líneas de abajo
+    # add the response (which is a csv row of mensaje,nombre,telefono) to the csv in utf-8
+    if is_admin:
+        print('es admin')
+        with open(mensajes_file, 'a', encoding='utf-8') as f:
+            #write the date, message and embedding
+            f.write(now.strftime("%d/%m/%Y %H:%M:%S")+'|'+message+'|'+str(message_vector)+'\n')
+
 
     return response
     
